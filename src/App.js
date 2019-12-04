1462099f782db86a4731b6129e4d6a7dfde8b564
Lauren Hetherington <laurenlhetherington@gmail.com>
2019-12-04 17:56:06 -0500

    Added a function that calls the yelp API, and then stores the returned businesses to state. :fire: :cat: :fish:

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.scss';
 import axios from 'axios';
+import Qs from 'qs';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Header from './Header.js';
 import SearchResults from './SearchResults.js';
@@ -9,7 +10,6 @@ import Footer from './Footer.js';
 
 class App extends Component {
   componentDidMount(){
-    
   }
   constructor(){
     super()
@@ -19,10 +19,42 @@ class App extends Component {
       stations: []
     }
   }
+
+  searchYelp = (searchLocation, sortBy='distance') => {
+    // Axios call for yelp data, uses Juno proxy
+    const yelpUrl = `https://api.yelp.com/v3/businesses/search`;
+    const yelpApiKey = `60l886Qycs9h_wC5Mg_GEdfIBdfzJ2oCL6-lPQcImfh57gu4W9udYJSt1QUdGFM-QXkwGEyNjJvkGAChBIT-4uupi7xVjjOucGT8XXXbirONqLZmbjC01vE4-BvnXXYx`;
+    axios({
+      url: 'https://proxy.hackeryou.com',
+      method:'GET',
+      dataResponse: 'json',
+      paramsSerializer: function(params) {
+        return Qs.stringify(params, {arrayFormat: 'brackets'})
+      },
+      params: {
+        reqUrl: yelpUrl,
+        proxyHeaders: {
+          'Authorization': `Bearer ${yelpApiKey}`
+        },
+        params: {
+          location: searchLocation,
+          sort_by: sortBy
+        },
+        xmlToJSON: false
+      }
+    }).then((response) => {
+      this.setState({
+        restaurants: response.data.businesses
+      })
+    })
+  }
+
   render(){
     return(
       <Router>
-        <Header/>
+        <Header
+          searchFunction={this.searchYelp}
+        />
         {(this.state.restaurants.length > 0) ? <Route exact path="/" component={SearchResults}/> : null}
         <Route path ="/restaurant/:id" component={RestaurantDetails}/>
         <Footer/>
