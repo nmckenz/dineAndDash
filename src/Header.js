c03e9e4037059184d410dd06f6b00e332063020e
Nicholas McKenzie <nicholas.a.mckenzie@gmail.com>
2019-12-04 18:27:40 -0500

    Placeholder images, search function.

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import "./partials/_Header.scss"
 
 class Header extends Component {
-    render(){
-        return(
+    constructor() {
+        super();
+        this.state = {
+            userInput: ""
+        }
+    }
+
+    handleChangeCityInput = (event) => {
+        this.setState({
+            userInput: event.target.value
+        });
+    };
+
+    render() {
+        return (
             <div>
-                <h1>Header</h1>
+                <div className="hero">
+                    <div className="heroContent">
+                        <h1>DINE AND DASH</h1>
+                        <fieldset>
+                            <form onSubmit={this.props.searchFunction(this.state.userInput)}>
+                                <input type="text" onChange={this.handleChangeCityInput} placeholder="Enter a city" />
+                                <button type="submit"></button>
+                            </form>
+                        </fieldset>
+                    </div>
+                </div>
+                <div className="restaurantRecommendation">
+                    <p>restaurant #1</p>
+                    <img src={require("./assets/restaurantRecommendation1Placeholder.jpg")} alt=""/>
+                </div>
+                <div className="restaurantRecommendation">
+                    <p>restaurant #2</p>
+                    <img src={require("./assets/restaurantRecommendation2Placeholder.jpg")} alt="" />
+                </div>
+                <div className="restaurantRecommendation">
+                    <p>restaurant #3</p>
+                    <img src={require("./assets/restaurantRecommendation3Placeholder.jpg")} alt="" />
+                </div>
             </div>
         )
     }
