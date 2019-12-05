9707a6213cfb60891918fda4df3fe583b08e2418
Pierce <nathanm0220@gmail.com>
2019-12-05 10:48:07 -0500

    changes to style

--- src/RestaurantDetails.js
+++ src/RestaurantDetails.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import './partials/RestaurantDetails.scss'
+import './partials/_RestaurantDetails.scss'
 
 
 class RestaurantDetails extends Component {
@@ -12,24 +12,20 @@ class RestaurantDetails extends Component {
 
     render() {
         return (
-            <div className="detailsContent">
-                <div className="wrapper">
+            <div className="wrapper">
+                <div className="detailsContent">
                     <div className="restaurantDetails">
                         <h2>Restaurant Title</h2>
-
-                        
                         <img src="https://via.placeholder.com/150" alt="" />
-
-                        
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga voluptatum cupiditate modi asperiores sequi at distinctio. Nulla tempore dolores quis assumenda earum ea, animi delectus aliquam dicta soluta voluptate dolore sed eos minus, cumque vel ex suscipit quae officia velit temporibus amet. Animi aspernatur sequi maiores optio impedit quasi alias.</p>
                         <p>rating:{}</p>
                         <p>(cuisineType){}</p>
                     </div>
                     <div className="bikeDetails">
-                        <img src="https://via.placeholder.com/150" alt=""/>
+                        <img src="https://via.placeholder.com/300" alt=""/>
                         <h2>Bikes Near You</h2>
                         <div className="bikeInfo">
-
+                            
                         </div>
                     </div>
                 </div>
