f577d77447e68ca7faba125193230f27f8a090db
Pierce <nathanm0220@gmail.com>
2019-12-05 12:17:58 -0500

    sdf

--- src/RestaurantDetails.js
+++ src/RestaurantDetails.js
@@ -3,6 +3,12 @@ import axios from 'axios';
 
 
 class RestaurantDetails extends Component {
+    constructor() {
+        super();
+        this.state = {
+            
+        };
+    }
 
 
 
@@ -10,20 +16,22 @@ class RestaurantDetails extends Component {
 
 
     render() {
+        console.log(this);
         return (
             <div className="wrapper">
                 <div className="detailsContent">
                     <div className="restaurantDetails">
                         <h2>Restaurant Title</h2>
                         <img src="https://via.placeholder.com/150" alt="" />
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga voluptatum cupiditate modi asperiores sequi at distinctio. Nulla tempore dolores quis assumenda earum ea, animi delectus aliquam dicta soluta voluptate dolore sed eos minus, cumque vel ex suscipit quae officia velit temporibus amet. Animi aspernatur sequi maiores optio impedit quasi alias.</p>
                         <p>rating:{}</p>
-                        <p>(cuisineType){}</p>
+                        <p>cuisineType{}</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga voluptatum cupiditate modi asperiores sequi at distinctio. Nulla tempore dolores quis assumenda earum ea, animi delectus aliquam dicta soluta voluptate dolore sed eos minus, cumque vel ex suscipit quae officia velit temporibus amet. Animi aspernatur sequi maiores optio impedit quasi alias.</p>
                     </div>
                     <div className="bikeDetails">
                         <img src="https://via.placeholder.com/300" alt=""/>
                         <h2>Bikes Near You</h2>
                         <div className="bikeInfo">
+                            <h3>placeholder text (bike share toronto)</h3>
                         </div>
                     </div>
                 </div>
