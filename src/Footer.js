95503c1a3a28a6ee201be0f62a030560612141cc
Anna <annatran93@gmail.com>
2019-12-10 16:01:00 -0500

    scroll to top button

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,41 @@
 import React, { Component } from 'react';
+// register fontawesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronCircleUp } from '@fortawesome/free-solid-svg-icons';
 
 class Footer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            is_visible: false
+        };
+    }
+    
+    componentDidMount() {
+        var scrollComponent = this;
+        document.addEventListener("scroll", function (e) {
+            scrollComponent.toggleVisibility();
+        });
+    } 
+
+    toggleVisibility() {
+        if (window.pageYOffset > 300) {
+            this.setState({
+                is_visible: true
+            });
+        } else {
+            this.setState({
+                is_visible: false
+            });
+        }
+    }
+    
+    scrollToTop() {
+        window.scrollTo(0, 0);
+    }
+
     render() {
+        const { is_visible } = this.state;
         return (
             <footer>
                 <div className="wrapper">
@@ -11,9 +45,11 @@ class Footer extends Component {
                         <a href="http://annattran.com/" target="_blank" rel="noopener noreferrer"> Anna</a>, 
                         <a href="https://piercemorales.com/" target="_blank" rel="noopener noreferrer"> Pierce</a>
                     </p>
-                    
-
                 </div>
+                {is_visible && (
+                <button class="scrollToTop" onClick={this.scrollToTop}>
+                    <FontAwesomeIcon icon={faChevronCircleUp} />
+                </button>)}                
             </footer>
         )
     }
