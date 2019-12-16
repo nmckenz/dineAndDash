dc87051843e4b82d2eab3db88dc233d58ffbfa95
Nicholas McKenzie <nicholas.a.mckenzie@gmail.com>
2019-12-15 19:19:43 -0500

    Updates

---
+++ precache-manifest.9b916de94b540fdc933a9c834e396d0f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10d3256624511ce2445445fee83c26a1",
+    "url": "/projectSix/index.html"
+  },
+  {
+    "revision": "6e8b1545143b3e8fa5c8",
+    "url": "/projectSix/static/css/main.cd73a500.chunk.css"
+  },
+  {
+    "revision": "fbf98948808c92a2c7c3",
+    "url": "/projectSix/static/js/2.1c416aca.chunk.js"
+  },
+  {
+    "revision": "0fe74914b459249c9b606864b87c3a76",
+    "url": "/projectSix/static/js/2.1c416aca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e8b1545143b3e8fa5c8",
+    "url": "/projectSix/static/js/main.69df6770.chunk.js"
+  },
+  {
+    "revision": "8f6cb45d46a4ba4bc0d9",
+    "url": "/projectSix/static/js/runtime-main.6feb3e74.js"
+  },
+  {
+    "revision": "2c0862a67bfb8773d52c7833e17cd5ca",
+    "url": "/projectSix/static/media/chevron.2c0862a6.svg"
+  },
+  {
+    "revision": "2092a7ba2e2be1e6824fce8878c903f4",
+    "url": "/projectSix/static/media/heroPlaceholder.2092a7ba.jpg"
+  },
+  {
+    "revision": "ceeeceaa062354b38243951cee9a1fd0",
+    "url": "/projectSix/static/media/imagePlaceholder.ceeeceaa.jpg"
+  },
+  {
+    "revision": "bab054c6a83f1cf8217511c4040716a1",
+    "url": "/projectSix/static/media/restaurantRecommendation1Placeholder.bab054c6.jpg"
+  },
+  {
+    "revision": "83b38e56794e02b909db752ea9021360",
+    "url": "/projectSix/static/media/restaurantRecommendation2Placeholder.83b38e56.jpg"
+  },
+  {
+    "revision": "e51818c5921bcb2747af6201ba31b761",
+    "url": "/projectSix/static/media/restaurantRecommendation3Placeholder.e51818c5.jpg"
+  },
+  {
+    "revision": "0653b58a593778df0d59f33cdb078fcf",
+    "url": "/projectSix/static/media/yelpLogo.0653b58a.png"
+  },
+  {
+    "revision": "adbe3936a57fa454811b989e9b0cd3b6",
+    "url": "/projectSix/static/media/yelpLogoIconOnly.adbe3936.png"
+  }
+]);
